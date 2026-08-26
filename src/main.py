a248b18aec9ef6f93ca5cfa9f709cf2f48ea787f
AydarGataullin7 <aydarcr7@gmail.com>
2026-08-26 15:00:26 +0500

    Add routes and fix frontend errors

--- src/main.py
+++ src/main.py
@@ -1,3 +1,32 @@
+import os
+
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+FRONTEND_DIR = os.path.join(BASE_DIR, "frontend")
+
+app.mount("/assets", StaticFiles(directory=os.path.join(FRONTEND_DIR, "assets")), name="assets")
+
+
+@app.get("/")
+def serve_index():
+    return FileResponse(os.path.join(FRONTEND_DIR, "index.html"))
+
+
+@app.get("/frontend-settings.json")
+def serve_settings():
+    return FileResponse(os.path.join(FRONTEND_DIR, "frontend-settings.json"))
+
+
+@app.get("/frontend-api/users/me")
+def get_current_user():
+    return {"username": "test_user", "email": "test@example.com"}
+
+
+@app.get("/vite.svg")
+def serve_vite_icon():
+    return FileResponse(os.path(FRONTEND_DIR, "vite.svg"))
