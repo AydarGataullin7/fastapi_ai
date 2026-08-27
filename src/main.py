076cba7edf00fdc9d1b7721138e946b82d712bac
AydarGataullin7 <aydarcr7@gmail.com>
2026-08-27 11:42:32 +0500

    Add Pydantic model

--- src/main.py
+++ src/main.py
@@ -3,6 +3,17 @@ import os
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
+from pydantic import BaseModel, Field
+
+
+class UserProfileResponse(BaseModel):
+    profileId: int = Field(description="Уникальный идентификатор пользователя")
+    email: str = Field(description="Email пользователя")
+    username: str = Field(min_length=3, max_length=20, description="Имя пользователя")
+    registeredAt: str = Field(description="Дата регистрации")
+    updatedAt: str = Field(description="Дата последнего обновления профиля")
+    isActive: bool = Field(description="Активен ли пользователь")
+
 
 app = FastAPI()
 
@@ -22,7 +33,7 @@ def serve_settings():
     return FileResponse(os.path.join(FRONTEND_DIR, "frontend-settings.json"))
 
 
-@app.get("/frontend-api/users/me")
+@app.get("/frontend-api/users/me", response_model=UserProfileResponse)
 def get_current_user():
     return {
         "profileId": 1,
